a3bb0ac608c206db3beaf153c596f36fb2094c37
Arata Tanaka <tarata43@yahoo.co.jp>
2016-06-26 23:32:07 +0900

    Fix count of getting tweet

--- src/main/scala/com/ru/waka/KusokoraRepository.scala
+++ src/main/scala/com/ru/waka/KusokoraRepository.scala
@@ -6,7 +6,7 @@ import scala.collection.JavaConverters._
 import scalaz.\/
 
 class KusokoraRepository(twitter: Twitter, private var urls: Seq[String] = Nil) {
-  private val query = new Query("#papixクソコラグランプリ")
+  private val query = new Query("#papixクソコラグランプリ").since("2016-04-01").count(100)
 
   def update(): Throwable \/ Unit = \/.fromTryCatchNonFatal({
     urls = twitter.search(query).getTweets.asScala.flatMap(tweet => {
